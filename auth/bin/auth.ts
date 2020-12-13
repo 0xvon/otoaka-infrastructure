7a117159d9e040eea18674a401dce4fff1d2b308
Masato TSUTSUMI <masatotsutsumi@masanyan-laptop.local>
2020-12-13 14:12:48 +0900

    environment variables

--- auth/bin/auth.ts
+++ auth/bin/auth.ts
@@ -7,7 +7,6 @@ const signinCallbackUrl = process.env.SIGNIN_CALLBACK_URL ? process.env.SIGNIN_C
 const signoutCallbackUrl = process.env.SIGNOUT_CALLBACK_URL ? process.env.SIGNOUT_CALLBACK_URL : 'https://sample.com';
 const googleWebClientId = process.env.GOOGLE_WEB_CLIENT_ID ? process.env.GOOGLE_WEB_CLIENT_ID : 'sample';
 const googleWebAppSecret = process.env.GOOGLE_WEB_APP_SECRET ? process.env.GOOGLE_WEB_APP_SECRET : 'sample';
-const appBundleId = process.env.APP_BUNDLE_ID ? process.env.APP_BUNDLE_ID : 'dev.sample.hoge';
 const facebookAppId = process.env.FACEBOOK_APP_ID ? process.env.FACEBOOK_APP_ID : 'sample';
 const facebookAppSecret = process.env.FACEBOOK_APP_SECRET ? process.env.FACEBOOK_APP_SECRET : 'dev.sample.hoge';
 
