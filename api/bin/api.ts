bdffc3f596e0ad753c8734c27c71818f9a8e077f
masatsch <kingofmasatojames@gmail.com>
2021-01-10 15:33:26 +0900

    separate env by branch

--- api/bin/api.ts
+++ api/bin/api.ts
@@ -16,7 +16,7 @@ const awsAccessKeyId = process.env.AWS_ACCESS_KEY_ID ?? 'HOGE';
 const awsSecretAccessKey = process.env.AWS_SECRET_ACCESS_KEY ?? 'HOGE';
 const awsRegion = process.env.AWS_REGION ?? 'ap-northeast-1';
 const cognitoUserPoolId = process.env.COGNITO_USER_POOL_ID ?? 'ap-northeast-1_cZhPmp0Td';
-const snsPlatformApplicationArn = process.env.SNS_PLATFORM_APPLICATION_ARN ?? '"arn:aws:sns:ap-northeast-1:960722127407:app/APNS_SANDBOX/rocket-ios-dev"';
+const snsPlatformApplicationArn = process.env.SNS_PLATFORM_APPLICATION_ARN ?? 'arn:aws:sns:ap-northeast-1:960722127407:app/APNS_SANDBOX/rocket-ios-dev';
 
 const app = new cdk.App();
 const vpcStack = new VPCStack(app, `${appName}-vpc`, {
@@ -26,36 +26,36 @@ const vpcStack = new VPCStack(app, `${appName}-vpc`, {
     },
 });
 
-const rdsStack = new RDSStack(app, `${appName}-rds`, {
-    appName,
-    dbname: rdsdbname,
-    username: rdsUserName,
-    password: rdsPassword,
-    vpc: vpcStack.vpc,
-    env: {
-        region: 'ap-northeast-1',
-    },
-});
+// const rdsStack = new RDSStack(app, `${appName}-rds`, {
+//     appName,
+//     dbname: rdsdbname,
+//     username: rdsUserName,
+//     password: rdsPassword,
+//     vpc: vpcStack.vpc,
+//     env: {
+//         region: 'ap-northeast-1',
+//     },
+// });
 
-const eksStack = new EKSStack(app, `${appName}-eks`, {
-    appName,
-    rdsSecurityGroupId: rdsStack.rdsSecurityGroupId,
-    clusterEndpoint: rdsStack.rds.clusterEndpoint.hostname,
-    dbname: rdsdbname,
-    rdsUsername: rdsUserName,
-    rdsPassword: rdsPassword,
-    awsAccessKeyId: awsAccessKeyId,
-    awsSecretAccessKey: awsSecretAccessKey,
-    snsPlatformApplicationArn: snsPlatformApplicationArn,
-    cognitoUserPoolId: cognitoUserPoolId,
-    awsRegion: awsRegion,
-    vpc: vpcStack.vpc,
-    githubOwner: githubOwner,
-    githubRepo: githubRepo,
-    githubBranch: githubBranch,
-    env: {
-        region: 'ap-northeast-1',
-    },
-});
+// const eksStack = new EKSStack(app, `${appName}-eks`, {
+//     appName,
+//     rdsSecurityGroupId: rdsStack.rdsSecurityGroupId,
+//     clusterEndpoint: rdsStack.rds.clusterEndpoint.hostname,
+//     dbname: rdsdbname,
+//     rdsUsername: rdsUserName,
+//     rdsPassword: rdsPassword,
+//     awsAccessKeyId: awsAccessKeyId,
+//     awsSecretAccessKey: awsSecretAccessKey,
+//     snsPlatformApplicationArn: snsPlatformApplicationArn,
+//     cognitoUserPoolId: cognitoUserPoolId,
+//     awsRegion: awsRegion,
+//     vpc: vpcStack.vpc,
+//     githubOwner: githubOwner,
+//     githubRepo: githubRepo,
+//     githubBranch: githubBranch,
+//     env: {
+//         region: 'ap-northeast-1',
+//     },
+// });
 
 app.synth();
