f2a95ed34f2d3c4a41cc8771e045f5a9ca1ea107
masatsch <kingmasatojames@gmail.com>
2021-11-06 18:49:31 +0900

    redirect

--- api/lib/ecs.ts
+++ api/lib/ecs.ts
@@ -65,7 +65,7 @@ export class ECSStack extends cdk.Stack {
             domainZone: HostedZone.fromLookup(this, 'hostZone', { domainName: props.config.domainZone }),
             domainName: props.config.domainName,
             certificate: Certificate.fromCertificateArn(this, 'certificate', props.config.acmCertificateArn),
-            redirectHTTP: false,
+            redirectHTTP: true,
         });
 
         if (props.mysqlSecurityGroupId) {
