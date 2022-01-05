46a7ce0069a90d3eabb549aadc8b40febaf9861b
masatsch <kingmasatojames@gmail.com>
2022-01-06 08:19:02 +0900

    fix

--- api/lib/rds.ts
+++ api/lib/rds.ts
@@ -123,7 +123,7 @@ export class RDSStack extends cdk.Stack {
                 vpc: this.props.vpc,
                 securityGroups: [rdsSecurityGroup],
                 autoMinorVersionUpgrade: true,
-                instanceType: InstanceType.of(InstanceClass.M4, InstanceSize.LARGE)
+                instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.LARGE)
             },
             cloudwatchLogsExports: [
                 'slowquery',
