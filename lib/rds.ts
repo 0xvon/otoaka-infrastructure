69b4160751d9899060f43a875f9256fba3199367
masatsch <kingmasatojames@gmail.com>
2022-03-07 09:23:00 +0900

    rds t3 large

--- lib/rds.ts
+++ lib/rds.ts
@@ -114,7 +114,7 @@ export class RDSStack extends cdk.Stack {
                 vpc: this.props.vpc,
                 securityGroups: [rdsSecurityGroup],
                 autoMinorVersionUpgrade: true,
-                instanceType: InstanceType.of(InstanceClass.R5, InstanceSize.XLARGE2)
+                instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.LARGE)
             },
             cloudwatchLogsExports: [
                 'slowquery',
